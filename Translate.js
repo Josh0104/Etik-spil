7a088b98a376a37500d287f4d52c12f1695fd555
Josh0104 <69515856+Josh0104@users.noreply.github.com>
2021-05-30 00:55:30 +0200

    change the p

---
+++ Translate.js
@@ -0,0 +1,19 @@
+function english(){
+    document.getElementById('p1').innerHTML = "I made this game to find out how a computer game can be ethically incorrect when it comes to the game's functional values ​​and aspects.";
+
+    document.getElementById('p2').innerHTML = "Note This game does not reflect my opinion in any case, and is made on the basis of my SOP project. The game here should help you understand how and when to push ethical boundaries by adding audiovisual elements.";
+
+    document.getElementById('p3').innerHTML = 'The game is made in three different versions. They all have the same mechanics where you control the blue car using the arrow keys or the "a" and "d" keys. The difference between the three games is their graphic elements.';
+
+    document.getElementById('p4').innerHTML = "Normal has the red car as a hostile element, and the gold chain as a point element.";
+
+    document.getElementById('p5').innerHTML ="Neutral has the black circle as a hostile element, and the white circle as a point element.";
+
+    document.getElementById('p6').innerHTML = "Extreme has the white man as a hostile element, and the black woman as a point element";
+
+    document.getElementById('p7').innerHTML ="Feel free to write a comment about the game to me or if there is a bug I need to fix. I'm going to an exam on ethics in computer games on Tuesday, June 1, 2021 :)";
+}
+
+function danish(){
+    
+}
