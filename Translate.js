f41f134f7fa6076ea49c6021c22cd6f110c6003e
Josh0104 <69515856+Josh0104@users.noreply.github.com>
2021-05-30 02:00:23 +0200

    Translation

--- Translate.js
+++ Translate.js
@@ -1,4 +1,4 @@
-function english(){
+function engelsk(){
     document.getElementById('p1').innerHTML = "I made this game to find out how a computer game can be ethically incorrect when it comes to the game's functional values ​​and aspects.";
 
     document.getElementById('p2').innerHTML = "Note This game does not reflect my opinion in any case, and is made on the basis of my SOP project. The game here should help you understand how and when to push ethical boundaries by adding audiovisual elements.";
@@ -12,8 +12,33 @@ function english(){
     document.getElementById('p6').innerHTML = "Extreme has the white man as a hostile element, and the black woman as a point element";
 
     document.getElementById('p7').innerHTML ="Feel free to write a comment about the game to me or if there is a bug I need to fix. I'm going to an exam on ethics in computer games on Tuesday, June 1, 2021 :)";
+
+    document.getElementById('danskButton').style.visibility = 'visible';
+    document.getElementById('engelskButton').style.visibility = 'hidden';
+
+
+
 }
 
-function danish(){
+function dansk(){
+
+    document.getElementById('p1').innerHTML = "Jeg har lavet dette spil for at finde ud af hvordan et computerspil kan være etisk ukorrekt, når det kommer til spillets funktionelle værdier og apsekter.";
+
+    document.getElementById('p2').innerHTML = "Dette spil afspejler ikke min mening under alle omstændigheder, og er lavet på baggrund af min SOP projekt. Spillet her skal hjælpe med at forstå hvordan og hvornår man rykker etiske grænser ved tilføjelse af audiovisuelle elementer.";
+
+    document.getElementById('p3').innerHTML = 'Spillet er lavet i tre forskellige versioner. De alle har den samme mekanik, hvor du styrer den blå bil ved hjælp af piletasterne eller tasterne "a" og "d". Forskellen mellem de tre spil er deres grafiske elementer.';
+
+    document.getElementById('p4').innerHTML = "Normal har den røde bil som en fjendtlig element, og guldkæden som en point element.";
+
+    document.getElementById('p5').innerHTML = "Neutral har den sorte cirkel som en fjendtlig element, og den hvide cirkel som en point element.";
+
+    document.getElementById('p6').innerHTML = "Ekstrem har den hvide mand som en fjendtlig element, og den sorte kvinde som en point element";
+
+    document.getElementById('p7').innerHTML = "Du må meget gerne skrive en kommentar om spillet til mig eller hvis der er en bug jeg skal fikse. Jeg skal nemlig til eksamen om etik i computerspil tirsdag d. 1. juni, 2021 :)"
     
+    document.getElementById('danskButton').style.visibility = 'hidden';
+    document.getElementById('engelskButton').style.visibility = 'visible';
 }
+
+document.getElementById('danskButton').style.visibility = 'hidden';
+console.log("feuwiohfuewi");
